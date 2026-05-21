a63705301283be3a1976b7135867a8c8430a3b14
leauhiic <loicvossier@gmail.com>
2026-05-21 22:08:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,144 @@ app.use(cors());
 
 const Tournois = [
   {
-    "name": "datadome",
-    "value": "1WICZHgcIcTaoVMPewpDfGDTUJPdepQTOFOxzeFgXHjHhajujeJf8OWFVVD_93Qx3csa9nVFYGxR01JMsX5DnF40P0mHo1UVyHN78Q96mAVEJ6r4HZ4jHBSxsYhKCPdS",
-    "domain": ".fft.fr",
-    "path": "/"
+    "Date tournoi": "27/07/2025",
+    "Nom": "tournoi interne P250",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Damien VILLEDIEU",
+    "Classement": 3,
+    "Point": 188,
+    "Validité": "juil-26"
+  },
+  {
+    "Date tournoi": "27/07/2025",
+    "Nom": "tournoi interne P250",
+    "type": "P250",
+    "Catégorie": "DX",
+    "Partenaire": "Elise SAVARIT",
+    "Classement": 4,
+    "Point": 180,
+    "Validité": "juil-26"
+  },
+  {
+    "Date tournoi": "30/01/2026",
+    "Nom": "P250 HOMMES",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Sébastien CREUSOT",
+    "Classement": 9,
+    "Point": 108,
+    "Validité": "janv-27"
+  },
+  {
+    "Date tournoi": "12/08/2025",
+    "Nom": "P250 H",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Damien VILLEDIEU",
+    "Classement": 9,
+    "Point": 108,
+    "Validité": "août-26"
+  },
+  {
+    "Date tournoi": "15/07/2025",
+    "Nom": "P250 HOMMES",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Mathieu SOUNIER",
+    "Classement": 9,
+    "Point": 108,
+    "Validité": "juil-26"
+  },
+  {
+    "Date tournoi": "16/01/2026",
+    "Nom": "p250 messieurs",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Clement TRICHARD",
+    "Classement": 6,
+    "Point": 100,
+    "Validité": "janv-27"
+  },
+  {
+    "Date tournoi": "28/01/2026",
+    "Nom": "Circuit M+ Matériaux Par Equipes - 4ème étape",
+    "type": "P100",
+    "Catégorie": "DM",
+    "Partenaire": "David SALA",
+    "Classement": 3,
+    "Point": 70,
+    "Validité": "janv-27"
+  },
+  {
+    "Date tournoi": "20/08/2025",
+    "Nom": "P250 MIXTE SOIREE",
+    "type": "P250",
+    "Catégorie": "DX",
+    "Partenaire": "Chloé CAIZERGUES",
+    "Classement": 11,
+    "Point": 63,
+    "Validité": "août-26"
+  },
+  {
+    "Date tournoi": "13/06/2025",
+    "Nom": "p250 hommes",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Vincent ROBERT",
+    "Classement": 9,
+    "Point": 63,
+    "Validité": "juin-26"
+  },
+  {
+    "Date tournoi": "06/06/2025",
+    "Nom": "P250H Mam's Padel",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Vincent ROBERT",
+    "Classement": 11,
+    "Point": 45,
+    "Validité": "juin-26"
+  },
+  {
+    "Date tournoi": "14/07/2025",
+    "Nom": "P100 MIXTE PADEL GENTLE LUNDI 14/07 MATIN",
+    "type": "P100",
+    "Catégorie": "DX",
+    "Partenaire": "Mathilde BENOIT",
+    "Classement": 5,
+    "Point": 35,
+    "Validité": "juil-26"
+  },
+  {
+    "Date tournoi": "16/02/2026",
+    "Nom": "P250H PUNTACO Padel",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Stéphane HUYGHE",
+    "Classement": 21,
+    "Point": 25,
+    "Validité": "févr-27"
+  },
+  {
+    "Date tournoi": "25/07/2025",
+    "Nom": "P250 HOMMES",
+    "type": "P250",
+    "Catégorie": "DM",
+    "Partenaire": "Vincent ROBERT",
+    "Classement": 21,
+    "Point": 25,
+    "Validité": "juil-26"
+  },
+  {
+    "Date tournoi": "24/02/2026",
+    "Nom": "P100 HOMMES",
+    "type": "P100",
+    "Catégorie": "DM",
+    "Partenaire": "Adrien EMERAUX",
+    "Classement": 18,
+    "Point": 18,
+    "Validité": "févr-27"
   }
 ];
 
