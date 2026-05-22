09ec77c5eab2177464c7b0e5f670a561e4dfd1c1
leauhiic <loicvossier@gmail.com>
2026-05-22 16:58:22 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
 
 
   useEffect(() => {
-    fetch(`${API}/tournois`)
+    fetch(`${API}/tournois2`)
       .then((res) => res.json())
       .then((data) => setTournois(data));
   }, []);
