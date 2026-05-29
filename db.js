ce82d0de2ca09270357df7591a3832158a10b1ad
leauhiic <loicvossier@gmail.com>
2026-05-29 09:20:00 +0200

    Harden PostgreSQL connection setup

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg");
 
-// Railway injecte automatiquement DATABASE_URL
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const config = {};
+
+if (process.env.DATABASE_URL) {
+  config.connectionString = process.env.DATABASE_URL;
+}
+
+if (process.env.PGSSL === "true") {
+  config.ssl = { rejectUnauthorized: false };
+}
+
+const pool = new Pool(config);
 
 module.exports = pool;
