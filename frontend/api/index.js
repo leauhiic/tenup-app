724ad0a85dc5bb42e9bd3e975007b512aa874bed
leauhiic <loicvossier@gmail.com>
2026-06-24 07:08:02 +0200

    Fix Vercel API dependency resolution

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,3 +1,14 @@
+const path = require("path");
+const Module = require("module");
+
+process.env.NODE_PATH = [
+  path.join(__dirname, "..", "node_modules"),
+  process.env.NODE_PATH,
+]
+  .filter(Boolean)
+  .join(path.delimiter);
+Module._initPaths();
+
 const app = require("../../server");
 
 module.exports = app;
